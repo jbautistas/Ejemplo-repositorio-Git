951c6dadbda3333d3bf6b94db986b343d4a8735d
juans <juans@DESKTOP-FOA59VT>
2020-11-29 13:38:35 -0500

    Se quito el letrero de “Hola mundo”

--- Proyecto/src/Main.java
+++ Proyecto/src/Main.java
@@ -3,7 +3,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hola mundo");
 		
 		System.out.println("Bienvenido a PROES");
 		System.out.println("Este es un espacio para que los profesores\npuedan venderse y los estudiantes puedan \nencontrar al profesor que buscan");
