e2434808a60259922afba2fa25ad6b2cfba87022
juans <juans@DESKTOP-FOA59VT>
2020-11-29 12:22:47 -0500

    Mensaje de bienvenida realizado sin GUI

--- Proyecto/src/Main.java
+++ Proyecto/src/Main.java
@@ -5,7 +5,8 @@ public class Main {
 		
 		System.out.println("Hola mundo");
 		
-		
+		System.out.println("Bienvenido a PROES");
+		System.out.println("Este es un espacio para que los profesores\npuedan venderse y los estudiantes puedan \nencontrar al profesor que buscan");
 	}
 
 }
